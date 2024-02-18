940a6816d8c2ff0cb8577fe898d9e1aff016a05c
Luccrivano <lucciano.rivano@gmail.com>
2024-02-18 11:06:52 -0300

    Elimination of yhe failed imports

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Formulario from './Formulario'
+import React from "react";
 
-
-const Alert = ({error,errorPass}) => {
-  // console.log("aca:",error) 
-  // console.log("aca:",errorPass) 
+const Alert = ({ error, errorPass }) => {
+  // console.log("aca:",error)
+  // console.log("aca:",errorPass)
   return (
     <>
-       {/* {console.log(mensaje)} */}
-       {error ? <p  className="error">Completa todos los campos</p> : <p className="error2">El Regidtro fue exitoso</p>}
-       {errorPass ? <p className="error">Las contraseñas no coinciden</p> : <p className="error2">ok</p>}  
-
-    
+      {/* {console.log(mensaje)} */}
+      {error ? (
+        <p className="error">Completa todos los campos</p>
+      ) : (
+        <p className="error2">El Regidtro fue exitoso</p>
+      )}
+      {errorPass ? (
+        <p className="error">Las contraseñas no coinciden</p>
+      ) : (
+        <p className="error2">ok</p>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
--- src/components/SocialButton.jsx
+++ src/components/SocialButton.jsx
@@ -1,25 +1,17 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook }  from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
-import Registro from './Registro'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+const SocialButton = ({ item }) => {
+  /*console.log("esta aca:",item.icon);*/
 
-
-const SocialButton = ({item}) => {
-    /*console.log("esta aca:",item.icon);*/
-  
-  
   return (
     <div>
-      <h1>Crea una cuenta</h1>  
-      <FontAwesomeIcon icon={item.icon1} className="fb" /> 
+      <h1>Crea una cuenta</h1>
+      <FontAwesomeIcon icon={item.icon1} className="fb" />
       <FontAwesomeIcon icon={item.icon2} className="git" />
-      <FontAwesomeIcon icon={item.icon3} className="in" />   
-      
+      <FontAwesomeIcon icon={item.icon3} className="in" />
     </div>
-  )
-}
+  );
+};
 
-export default SocialButton
+export default SocialButton;
