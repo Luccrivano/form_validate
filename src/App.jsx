326fdcab750e4b2a1b36e6f40c72a5b8007f4f21
Luccrivano <lucciano.rivano@gmail.com>
2024-02-18 11:17:17 -0300

    Add Css

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import "./App.css";
 import Registro from "./components/Registro";
 //import Formulario from "./components/Formulario";
 
-function App({item}) {
+function App({ item }) {
   return (
-    <>
-      
+    <div className="rrss">
       <Registro />
-      
+
       {/* <Socialbutton /> */}
-    </>
+    </div>
   );
 }
 
--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,29 +1,27 @@
 import React from "react";
-import { faFacebook }  from '@fortawesome/free-brands-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 /*import { useState } from 'react'
 import { faHome } from '@fortawesome/free-solid-svg-icons'
 import { faWifi } from '@fortawesome/free-solid-svg-icons';*/
 
 import Formulario from "./Formulario";
 
-import SocialButton from "./SocialButton"
+import SocialButton from "./SocialButton";
 
 const miItem = {
-  icon1:faFacebook,
-  icon2:faGithub,
-  icon3:faLinkedinIn
-}
+  icon1: faFacebook,
+  icon2: faGithub,
+  icon3: faLinkedinIn,
+};
 
 const Registro = () => {
   return (
-    
     <div>
-      <SocialButton item={miItem}/> 
+      <SocialButton item={miItem} />
       <h5> usa tu email para registrarte</h5>
       <Formulario />
- 
     </div>
   );
 };
